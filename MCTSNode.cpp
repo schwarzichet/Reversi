0fa164d7602f22760bff63266ff6c4e47042b643
Crossady <785436364@qq.com>
2018-04-27 23:12:11 +0800

    child->father raw; father->child shared_ptr

--- MCTSNode.cpp
+++ MCTSNode.cpp
@@ -17,9 +17,9 @@ bool MCTSNode::isAllExpanded() {
 
 }
 
-MCTSNode *MCTSNode::bestChild() {
+std::shared_ptr<MCTSNode> MCTSNode::bestChild() {
     double max = 0;
-    MCTSNode *bestChild = nullptr;
+    std::shared_ptr<MCTSNode> bestChild;
     for (auto child  : children) {
         double childUCB =
                 child->wonTimes / child->visitTimes + MCTSNode::C * sqrt(log(this->visitTimes / child->visitTimes));
@@ -32,7 +32,7 @@ MCTSNode *MCTSNode::bestChild() {
     return bestChild;
 }
 
-MCTSNode *MCTSNode::randomUnexpandedChild() {
+std::shared_ptr<MCTSNode> MCTSNode::randomUnexpandedChild() {
     std::random_device r;
     size_t mean;
     do {
@@ -46,14 +46,13 @@ MCTSNode *MCTSNode::randomUnexpandedChild() {
 
 void MCTSNode::genChildren() {
     for (auto step : this->boardState.possibleMovement(color)) {
-        MCTSNode *child = new MCTSNode(this, this->boardState.next(step.first, step.second, color), -color);
+        auto child = std::make_shared<MCTSNode>(this, this->boardState.next(step.first, step.second, color), -color);
         children.push_back(child);
     }
-    //TODO: need delete.....
 }
 
 
-MCTSNode *selection(MCTSNode *node) {
+std::shared_ptr<MCTSNode> selection(std::shared_ptr<MCTSNode> node) {
     while (node->children.size() == 0) {
         if (node->isAllExpanded()) {
             return selection(node->bestChild());
@@ -64,7 +63,7 @@ MCTSNode *selection(MCTSNode *node) {
 }
 
 
-MCTSNode *expasion(MCTSNode *node) {
+std::shared_ptr<MCTSNode> expansion(std::shared_ptr<MCTSNode> node) {
     if (node->boardState.isTerminal()) {
         return node;
     } else {
@@ -73,7 +72,7 @@ MCTSNode *expasion(MCTSNode *node) {
     }
 }
 
-MCTSNode *simulatoin(MCTSNode *node) {
+std::shared_ptr<MCTSNode> simulation(std::shared_ptr<MCTSNode> node) {
     if (node->boardState.isTerminal()) { return node; }
     else {
         return node->completeGameRandomly();
@@ -91,9 +90,9 @@ void backpropagation(MCTSNode *node) {
     }
 }
 
-MCTSNode *MCTSNode::completeGameRandomly() {
+std::shared_ptr<MCTSNode> MCTSNode::completeGameRandomly() {
 //    return nullptr;
-    MCTSNode *node = this;
+    auto node = std::shared_ptr<MCTSNode>(this);
     while (!node->boardState.isTerminal()) {
         auto possibleMove = node->boardState.possibleMovement(node->color);
         std::random_device r;
@@ -101,11 +100,41 @@ MCTSNode *MCTSNode::completeGameRandomly() {
         std::uniform_int_distribution<size_t> uniform_dist(0, possibleMove.size() - 1);
         size_t mean = uniform_dist(e1);
         auto board = node->boardState.next(possibleMove[mean].first, possibleMove[mean].second, color);
-        node = new MCTSNode(node, board, -color);
+        node = std::make_shared<MCTSNode>(node.get(), board, -color);
     }
-    node->win = node->boardState.setWin(color);
+    node->win = node->boardState.isWin(color);
 
     return node;
 }
 
+std::shared_ptr<MCTSNode> MCTSNode::decideNext() {
+    double max = 0;
+    std::shared_ptr<MCTSNode> bestChild = nullptr;
+    for (auto child  : children) {
+        if (child->visitTimes == 0) {
+            continue;
+        }
+        double childUCB =
+                child->wonTimes / child->visitTimes + MCTSNode::C * sqrt(log(this->visitTimes / child->visitTimes));
+        if (childUCB > max) {
+            max = childUCB;
+            bestChild = child;
+        }
+    }
+    bestChild->father = nullptr;
+    assert(bestChild);
+    return bestChild;
+}
 
+std::shared_ptr<MCTSNode> MCTSNode::selectNext(int row, int column) {
+    if (this->children.size() == 0) {
+        genChildren();
+    }
+    for (auto child : children) {
+        if (child->boardState.getBoard()[row][column] == color) {
+            return child;
+        }
+    }
+    assert(false);
+    return nullptr;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "Reversi.h"
 #include "MCTSNode.h"
 #include <cstdio>
@@ -8,12 +9,23 @@ int main() {
     std::cout << "Hello!" << std::endl;
     Reversi r = Reversi();
     r.printBoard();
-    MCTSNode* root;
-    while (!r.isTerminal()) {
-        int row = 0, column = 0, color = 0;
-        std::cin >> row >> column >> color;
-        r = r.next(row, column, color);
-        r.printBoard();
+    auto root = std::make_shared<MCTSNode>(nullptr, r, BLACK);
+    while (!root->boardState.isTerminal()) {
+
+        int row = 0, column = 0;
+        std::cin >> row >> column;
+//        Reversi newBoard = root->boardState.next(row, column, root->color);
+//        root = new MCTSNode(nullptr, newBoard, -root->color);
+        root = root->selectNext(row, column);
+
+
+        auto leaf  = selection(root);
+        auto newChild = expansion(leaf);
+        auto result = simulation(newChild);
+        backpropagation(result.get());
+        root = root->decideNext();
+//        r = r.next(row, column, color);
+//        r.printBoard();
 //        root = new MCTSNode(nullptr, r, )
     }
     return 0;
